99a1e51f722e67502ddb0b7362f68b3bcd2cd09f
Karthik Bahaddurgatte Matha <karthikbahaddurgatte.matha@ibm.com>
2025-09-05 11:31:26 +0530

    Added 180 sec delay

--- bookController.js
+++ bookController.js
@@ -4,44 +4,54 @@ const bookService = require('./bookService');
 
 // GET /api/books
 router.get('/', (req, res) => {
-    const books = bookService.getAllBooks();
-    res.json(books);
+    setTimeout(() => {
+        const books = bookService.getAllBooks();
+        res.json(books);
+    }, 180000); // 180 secs
 });
 
 // GET /api/books/{id}
 router.get('/:id', (req, res) => {
-    const book = bookService.getBookById(parseInt(req.params.id));
-    if (book) {
-        res.json(book);
-    } else {
-        res.status(404).send('Book not found');
-    }
+    setTimeout(() => {
+        const book = bookService.getBookById(parseInt(req.params.id));
+        if (book) {
+            res.json(book);
+        } else {
+            res.status(404).send('Book not found');
+        }
+    }, 180000); // 180 secs
 });
 
 // POST /api/books
 router.post('/', (req, res) => {
-    const newBook = bookService.createBook(req.body);
-    res.status(201).json(newBook);
+    setTimeout(() => {
+        const newBook = bookService.createBook(req.body);
+        res.status(201).json(newBook);
+    }, 180000); // 180 secs
 });
 
 // PUT /api/books/{id}
 router.put('/:id', (req, res) => {
-    const updatedBook = bookService.updateBook(parseInt(req.params.id), req.body);
-    if (updatedBook) {
-        res.json(updatedBook);
-    } else {
-        res.status(404).send('Book not found');
-    }
+    setTimeout(() => {
+        const updatedBook = bookService.updateBook(parseInt(req.params.id), req.body);
+        if (updatedBook) {
+            res.json(updatedBook);
+        } else {
+            res.status(404).send('Book not found');
+        }
+    }, 180000); // 180 secs
 });
 
 // DELETE /api/books/{id}
 router.delete('/:id', (req, res) => {
-    const wasDeleted = bookService.deleteBook(parseInt(req.params.id));
-    if (wasDeleted) {
-        res.status(204).send(); // No content
-    } else {
-        res.status(404).send('Book not found');
-    }
+    setTimeout(() => {
+        const wasDeleted = bookService.deleteBook(parseInt(req.params.id));
+        if (wasDeleted) {
+            res.status(204).send(); // No content
+        } else {
+            res.status(404).send('Book not found');
+        }
+    }, 180000); // 180 secs
 });
 
 module.exports = router;
